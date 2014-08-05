e8d4fcfe22ce9c9b0b7b51762b4f25bea6537ed0
Fei Lee <lee.lfff@gmail.com>
2014-08-05 15:42:51 +0800

    add sth

--- proxy/runproxy.sh
+++ proxy/runproxy.sh
@@ -3,8 +3,9 @@
 #startup proxy
 
 #set ip address
-#export BACKEND=$(cat /etc/hosts | grep mysql | cut -f1)
+#export MYSQL_IP=$(cat /etc/hosts | grep mysql | cut -f1)
 export PROXY=$(cat /etc/hosts | head -n1 | cut -f1)
+#export PROXY=$(hostname -i)
 
 ./bins/proxy-bin/bin/mysql-proxy \
 	--plugins=proxy --event-threads=4 \
